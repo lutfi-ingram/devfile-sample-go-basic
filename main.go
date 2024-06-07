a5a83a2a7c6fb52f29cfc57832a164bac71ef8ba
lutfi-ingram <67720910+lutfi-ingram@users.noreply.github.com>
2024-06-07 16:02:46 +0700

    Update main.go create upload

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		port = "8080"
 	}
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/upload", uploadHandler)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 }
 
@@ -21,6 +22,40 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	if path != "" {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	} else {
-		fmt.Fprint(w, "Hello World!")
+		fmt.Fprint(w, "Hello World!<br/><form action='/upload' method='post' enctype='multipart/form-data'>
+        <input type='file' name='file' accept='.txt'>
+        <input type='submit' value='Upload'>")
+	}
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		file, _, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "Error reading file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		// Create a new file in the specified directory
+		uploadPath := "/usr/share/external_persistent"
+		filename := "uploaded_file.txt"
+		dst, err := os.Create(uploadPath + filename)
+		if err != nil {
+			http.Error(w, "Error creating file", http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		// Copy the uploaded file content to the new file
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			http.Error(w, "Error saving file", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintln(w, "File uploaded successfully!")
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
